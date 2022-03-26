160b292a9f96d1b4bf724bc3d5adfd2fb7b4f90e
Cuong Nguyen <nqcuong.1710@gmail.com>
2022-03-26 23:57:38 +0700

    create database

--- src/seeders/20220324194634-demo-use.js
+++ src/seeders/20220324194634-demo-use.js
@@ -1,11 +1,22 @@
 'use strict';
-
+// email: DataTypes.STRING,
+//     password: DataTypes.STRING,
+//     firstName: DataTypes.STRING,
+//     lastName: DataTypes.STRING,
+//     address : DataTypes.STRING,
+//     gender: DataTypes.BOOLEAN,
+//     roleid: DataTypes.STRING
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'Nguyen Quoc',
+      lastName: 'Cuong',
+      address: 'Da Nang',
+      gender: 1,
+      typeRole: "ROLE",
+      keyRole: "R1",
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
